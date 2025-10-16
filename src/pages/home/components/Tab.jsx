8cbf0e83ba925e4f203e5f73156df9b5595a93f4
Imashaperera09 <imashaperera09@gmail.com>
2025-10-16 23:12:01 +0530

    changes

--- src/pages/home/components/Tab.jsx
+++ src/pages/home/components/Tab.jsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { useSelector } from "react-redux";
+import { switchHomeTab } from "@/lib/redux/features/uiSlice";
+import { useDispatch } from "react-redux";
 
 const Tab = (props) => {
+  const dispatch = useDispatch();
+  const selectedTab = useSelector((state) => state.ui.selectedHomeTab);
+
   return (
     <Button
       key={props.tab.value}
-      variant={props.selectedTab === props.tab.value ? "default" : "outline"}
-      onClick={() => props.onClick(props.tab.value)}
+      variant={selectedTab === props.tab.value ? "default" : "outline"}
+      onClick={() => dispatch(switchHomeTab(props.tab.value))}
     >
       {props.tab.label}
     </Button>
