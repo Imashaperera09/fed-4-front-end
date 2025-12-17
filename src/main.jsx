4508b564f38ad3a4517984ed9ce9cf35fff1e97e
Imasha Perera <imashaperera09@gmail.com>
2025-12-17 21:46:39 +0530

    authentication

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ import DashboardPage from './pages/dashboard/dashboard.page.jsx'
 import RootLayout from './layouts/rootLayout.jsx'
 import MainLayout from './layouts/main.layout.jsx'
 import DashboardLayout from './layouts/dashboard.layout.jsx'
-import { ClerkProvider } from '@clerk/react-router'
+import SignInPage from './pages/auth/sign-in-page.jsx'
+import SignUpPage from './pages/auth/sign-up-page.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -20,22 +22,30 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+      afterSignInUrl="/"
+      afterSignUpUrl="/"
+    >
       <Provider store={store}>
         <BrowserRouter>
-          <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-            <Routes>
-              <Route element={<RootLayout />}>
-                <Route element={<MainLayout />}>
-                  <Route path="/" element={<HomePage />} />
-                </Route>
-                <Route element={<DashboardLayout />}>
-                  <Route path="/dashboard" element={<DashboardPage />} />
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/sign-in" element={<SignInPage />} />
+              <Route path="/sign-up" element={<SignUpPage />} />
+              <Route element={<MainLayout />}>
+                <Route index element={<HomePage />} />
               </Route>
+              <Route element={<DashboardLayout />}>
+                <Route path="/dashboard" element={<DashboardPage />} />
               </Route>
-            </Routes>
-          </ClerkProvider>
+            </Route>
+          </Routes>
         </BrowserRouter>
       </Provider>
-    </StrictMode>
+    </ClerkProvider>
+  </StrictMode>
 );
 
