c60f66d8a3205b07b9cad48bfbc6b9cbe1fb93a1
Imasha Perera <imashaperera09@gmail.com>
2025-12-21 22:52:10 +0530

    changes

--- src/lib/redux/query.js
+++ src/lib/redux/query.js
@@ -7,25 +7,47 @@ export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: baseUrl, prepareHeaders: async (headers) => {
     const clerk = window.Clerk;
-    if (!clerk) {
-      const token = await clerk.session.baseUrlgetToken();
-      console.log( token);
+    if (clerk) {
+      const token = await clerk.session.getToken();
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
     }
     return headers;
-  }}),
+  } }),
   endpoints: (build) => ({
     getEnergyGenerationRecordsBySolarUnit: build.query({
-      query: ({id, groupBy}) => `/energy-generation-records/solar-unit/${id}?groupBy=${groupBy}`,
+      query: ({id, groupBy, limit}) => `/energy-generation-records/solar-unit/${id}?groupBy=${groupBy}&limit=${limit}`,
     }),
-    getSolarUnitByClerkUserId: build.query({
-      query: (clerkUserId) => `/solar-units/users/${clerkUserId}`,
+    getSolarUnitForUser: build.query({
+      query: () => `/solar-units/me`,
+    }),
+    getSolarUnits: build.query({
+      query: () => `/solar-units`,
+    }),
+    getSolarUnitById: build.query({
+      query: (id) => `/solar-units/${id}`,
+    }),
+    createSolarUnit: build.mutation({
+      query: (data) => ({
+        url: `/solar-units`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    editSolarUnit: build.mutation({
+      query: ({id, data}) => ({
+        url: `/solar-units/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getAllUsers: build.query({
+      query: () => `/users`,
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetEnergyGenerationRecordsBySolarUnitQuery, useGetSolarUnitByClerkUserIdQuery } = api;
+export const { useGetAllUsersQuery, useGetEnergyGenerationRecordsBySolarUnitQuery, useGetSolarUnitForUserQuery, useGetSolarUnitsQuery, useGetSolarUnitByIdQuery, useCreateSolarUnitMutation, useEditSolarUnitMutation } = api;
